cabf9532be010c71ef09986731e6a8e578911101
martinb <martinbraun123@aol.com>
2020-04-13 19:16:11 +0200

    minor

--- certs/pg_auto_failover/recreate_server_certs.sh
+++ certs/pg_auto_failover/recreate_server_certs.sh
@@ -6,6 +6,8 @@ bash gen_server_cert.sh root_ca af-monitor-01/postgres_server 116.203.218.49 yJp
 bash gen_server_cert.sh root_ca af-database-01/postgres_server 116.203.218.75 4wNxA9Uaut5BtMTR3tdExU5xGGMRs4p4
 bash gen_server_cert.sh root_ca af-database-02/postgres_server 49.12.9.15 zkF8SU8Q2uNz3bBsJQ94F6Gx5DbcEgJ5
 
+mkdir -p ../../files/certs
+
 rm -rf ../../files/certs/af-monitor-01
 rm -rf ../../files/certs/af-database-01
 rm -rf ../../files/certs/af-database-02
